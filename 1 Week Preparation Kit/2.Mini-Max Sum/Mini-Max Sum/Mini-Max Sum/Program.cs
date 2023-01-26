dc8c814f1e65f71ac6b64e72c8a0ce1d57b7ac3c
Rahil Mehta <65954612+Karma-Yogi-Rahil@users.noreply.github.com>
2023-01-26 22:27:16 +0530

    Working

---
+++ 1 Week Preparation Kit/2.Mini-Max Sum/Mini-Max Sum/Mini-Max Sum/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Mini_Max_Sum
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            List<int> arr =new List<int>();
+            arr.Add(1);
+            arr.Add(2);
+            arr.Add(3);
+            arr.Add(4);
+            arr.Add(5);
+
+            MiniMaxSum(arr);
+        }
+
+        public static void MiniMaxSum(List<int> arr)
+        {
+            int totalSum = 0;
+            int maxSum = 0;
+            int minSum = 0;
+
+            foreach(int number in arr)
+            {
+                totalSum += number;
+            }
+
+            foreach(int number in arr)
+            {
+                int temptotal = totalSum - number;
+                if (maxSum < temptotal)
+                {
+                    maxSum = temptotal;
+                }
+
+                if (minSum > temptotal)
+                {
+                    minSum = temptotal;
+                }
+            }
+
+            Console.WriteLine(minSum + maxSum);
+
+        }
+
+    }
+}
