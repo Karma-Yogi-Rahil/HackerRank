3052181e47de6ae73cdac0feaf13edcbcd61a750
Rahil Mehta <65954612+Karma-Yogi-Rahil@users.noreply.github.com>
2023-02-02 17:35:46 +0530

    Completed Lonely Integer

---
+++ 2 Week Preparation Kit/Lonely Integer/LonelyInteger/LonelyInteger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace LonelyInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>(7) { 1, 2, 3, 4, 3, 2, 1 };
+            int result = lonelyinteger(a);
+
+            Console.WriteLine(result);
+
+        }
+
+        public static int lonelyinteger(List<int> a)
+        {
+            int result = 0;
+
+            // Using XOR operation.
+            foreach(int i in a)
+            {
+                result ^= i;
+
+            }
+
+            return result;
+        }
+    }
+}
